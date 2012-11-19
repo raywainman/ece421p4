a60312ab57393af1c0ec2d7383970dc49398964c
Kenneth Rodas <krodas@ualberta.ca>
2012-11-19 08:20:57 -0700

    More gui changes

--- Gui Prototype/Holla.rb
+++ Gui Prototype/Holla.rb
@@ -3,32 +3,40 @@ require 'gtk2'
 
 class HelloGlade
    
-  attr :glade
-   
+ 
+  
   def initialize  
       Gtk.init 
-      builder = Gtk::Builder::new
-      builder.add_from_file("tutorial.glade") 
-      builder.connect_signals{ |handler| method(handler) } 
-      window = builder.get_object("mainMenu") 
+      @builder = Gtk::Builder::new
+      @builder.add_from_file("tutorial.glade") 
+      @builder.connect_signals{ |handler| method(handler) } 
+      window = @builder.get_object("mainMenu")
+      self.get_all_objects() 
       window.show() 
-      Gtk.main() 
+      Gtk.main()
+      @spinner_activated=false
   end
   
-  def on_button1_clicked(widget)
-    puts "hello world"
+  def get_all_objects
+    @spinner1=@builder.get_object("spinner1")
   end
   
   def on_button2_clicked(widget)
-    exit(0)
+    Gtk.main_quit
   end
   
-  def on_imagemenuitem1_activate_item(widget)
-    exit(0)
+  def on_spinbutton1_change_value(widget)
+    #nothing
   end
   
-  def on_checkbutton1_toggled(widget)
-    textview1.text="res"
+  def on_button1_clicked(widget)
+    @spinner_activated=!@spinner_activated
+    @spinner1.active=@spinner_activated
+  end
+  
+  def on_button3_clicked
+    @spinner1.visible=false
+    puts "hidden"
   end
  
 end
